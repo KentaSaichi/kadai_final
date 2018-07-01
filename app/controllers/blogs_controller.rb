a403d0d02347f85d631d839ed229012cfb3dd0c8
Kenta Saichi <kenta.saichi@pwc.com>
2018-07-01 12:17:34 +0000

    origin

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,11 @@ class BlogsController < ApplicationController
   end
 
   def new
-    @blog = Blog.new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
   end
 
   def create
@@ -42,6 +46,7 @@ class BlogsController < ApplicationController
   end
   def confirm
     @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
   end
 
   private
